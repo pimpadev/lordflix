dcf16e5a6af57d6cb02177e9f247e06c7235f4cd
Adrián Ferrando <adri.vlc91@gmail.com>
2024-10-22 00:37:51 +0200

    Using a real placeholder

--- scripts/clean_logs.sh
+++ scripts/clean_logs.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# Specify the path to the logs directory (change this to your absolute path)
-LOG_DIRECTORY="/home/<USER>/lordflix/logs"
+# Specify the path to the logs directory using the $USER variable
+LOG_DIRECTORY="/home/$USER/lordflix/logs"
 
 # Verify the path
 echo "Checking the logs directory: $LOG_DIRECTORY"
@@ -14,7 +14,7 @@ if [ -d "$LOG_DIRECTORY" ]; then
         if [ -f "$LOG_FILE" ]; then
             # Get the file's age in seconds
             FILE_AGE=$(($(date +%s) - $(stat -c %Y "$LOG_FILE")))
-            
+
             # Check if the log file is older than 60 days (5,184,000 seconds)
             if [ "$FILE_AGE" -gt 5184000 ]; then
                 echo "The log file '$LOG_FILE' is older than 60 days and will be deleted."
